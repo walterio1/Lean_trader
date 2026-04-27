862582a231d7fd838de5343ea12c0726f377b4b8
walter scherk <walterscherk@yahoo.com>
2026-04-27 19:09:57 +0200

    entrar state (pend vix)
    Co-authored-by: Bryan G. Cabrera Ramirez <bryanitou@users.noreply.github.com>

--- custom-data_buy-sma/main.py
+++ custom-data_buy-sma/main.py
@@ -12,7 +12,12 @@ class SmaStrategy(QCAlgorithm):
         self.set_end_date(2010, 10, 11)
         self.set_cash(100000)
 
-        self.symbol = self.add_data(SPFut, "SPFut", Resolution.MINUTE).symbol
+        # Local variables
+        self.state = False
+
+        # Add symbols
+        self.symbol_SPFut = self.add_data(SPFut, "SPFut", Resolution.MINUTE).symbol
+        self.symbol_VIXSlope = self.add_data(VIXSlope, "VIXSlope", Resolution.DAILY).symbol
         
         # SMA period
         self.sma_period = 8*60*30  # 30 trading days of 8 hours each
@@ -30,31 +35,37 @@ class SmaStrategy(QCAlgorithm):
         if self.is_warming_up:
             return
 
-        if not data.contains_key(self.symbol):
-            return
+        if data.contains_key(self.symbol_VIXSlope):
+            # Compute state
+            self.state = data[self.symbol_VIXSlope].open > data[self.symbol_VIXSlope].close
+            self.plot("SMA Chart", "VIX Slope - Value 1", data[self.symbol_VIXSlope].open)
+            self.plot("SMA Chart", "VIX Slope - Value 2", data[self.symbol_VIXSlope].close)
+            self.plot("SMA Chart", "VIX Slope - State", int(self.state))
 
-        price = data[self.symbol].close
 
-        self.sma.update(self.time, price)
+        if data.contains_key(self.symbol_SPFut):
+            price = data[self.symbol_SPFut].close
 
-        # Ensure SMA is ready
-        if not self.sma.is_ready:
-            return
+            self.sma.update(self.time, price)
 
-        sma_value = self.sma.current.value
+            # Ensure SMA is ready
+            if not self.sma.is_ready:
+                return
 
-        self.plot("SMA Chart", "Price", price)
-        self.plot("SMA Chart", "SMA", sma_value)
+            sma_value = self.sma.current.value
 
-        # Buy condition
-        if price > sma_value and not self.portfolio.invested:
-            self.set_holdings(self.symbol, 1)
-            self.debug(f"BUY @ {price} | SMA: {sma_value}")
+            self.plot("SMA Chart", "Price", price)
+            self.plot("SMA Chart", "SMA", sma_value)
 
-        # Sell condition
-        elif price < sma_value and self.portfolio.invested:
-            self.liquidate(self.symbol)
-            self.debug(f"SELL @ {price} | SMA: {sma_value}")
+            # Buy condition
+            if price > sma_value and not self.portfolio.invested:
+                self.set_holdings(self.symbol_SPFut, 1)
+                self.debug(f"BUY @ {price} | SMA: {sma_value}")
+
+            # Sell condition
+            elif price < sma_value and self.portfolio.invested:
+                self.liquidate(self.symbol_SPFut)
+                self.debug(f"SELL @ {price} | SMA: {sma_value}")
 
 
 class SPFut(PythonData):
@@ -84,6 +95,42 @@ class SPFut(PythonData):
             index["Low"] = float(data[3])
             index["Close"] = float(index.value)
             index.volume = int(data[5])
+        except ValueError:
+            return None
+
+        return index
+    
+
+class VIXSlope(PythonData):
+    '''VIX SLope Custom Data Class'''
+    def get_source(self, config, date, is_live_mode):
+        return SubscriptionDataSource(os.path.join(Globals.data_folder, "custom", "Preu123.csv"), SubscriptionTransportMedium.LOCAL_FILE, FileFormat.CSV)
+
+    def reader(self, config, line, date, is_live_mode):
+        if not (line.strip() and line[0].isdigit()): return None
+
+        # New Nifty object
+        index = VIXSlope()
+        index.symbol = config.symbol
+        # index.data_type = MarketDataType.AUXILIARY
+
+        try:
+            # Example File Format:
+            # date,first,second,third,slope,contract,incFirst,incSecond
+            # 2008-01-02,23.91,23.9,Q08,-0.000418235,Q08,,
+            # 2008-01-03,23.77,23.92,Q08,0.006310475,Q08,-0.005855291,0.00083682
+            # 2008-01-04,24.42,24.35,Q08,-0.002866503,Q08,0.027345393,0.017976589
+            data = line.split(',')
+            index.time = datetime.strptime(data[0], "%Y-%m-%d")
+            index.end_time = index.time + timedelta(days=1)
+            # index.value = float(data[4])
+            index["Open"] = float(data[1])
+            index["Close"] = float(data[2])
+            # index["Open"] = float(data[1])
+            # index["High"] = float(data[2])
+            # index["Low"] = float(data[3])
+            # index["Close"] = float(index.value)
+            # index.volume = int(data[5])
         except ValueError:
             return None
 
